411fdfe1e6b27908ddc995d18270f91898069520
altescape <hello@michaelwatts.me>
2015-02-16 14:36:19 +0000

    Class fun

--- app/dist/mySpec.js
+++ app/dist/mySpec.js
@@ -12,27 +12,57 @@
   });
 
   describe("how to build a class", function() {
-    return it("car should have 4 wheels...", function() {
-      var Car, c;
-      Car = (function() {
-        Car.prototype.wheels = 4;
+    var Car, Engine;
+    Car = (function() {
+      Car.prototype.wheels = 4;
 
-        Car.prototype.engine = void 0;
+      Car.prototype.engine = void 0;
 
-        function Car(name) {
-          this.fit_engine = __bind(this.fit_engine, this);
-          this.name = name;
-        }
+      function Car(name) {
+        this.fit_engine = __bind(this.fit_engine, this);
+        this.name = name;
+      }
 
-        Car.prototype.fit_engine = function(engine) {
-          return this.engine = engine;
-        };
+      Car.prototype.fit_engine = function(engine) {
+        return this.engine = engine;
+      };
 
-        return Car;
+      return Car;
 
-      })();
+    })();
+    Engine = (function() {
+      Engine.prototype.power = 300;
+
+      Engine.prototype.sound_level = 9;
+
+      function Engine(name) {
+        this.name = name;
+      }
+
+      return Engine;
+
+    })();
+    it("car should return correct default properties...", function() {
+      var c;
       c = new Car;
       return expect(c.wheels).toBe(4);
+    });
+    it("should return correct name", function() {
+      var c;
+      c = new Car;
+      c.name = "Mobile";
+      return expect(c.name).toBe("Mobile");
+    });
+    return it("should have an engine and and return its name", function() {
+      var c, e;
+      c = new Car;
+      c.name = "The Beast";
+      e = new Engine("Roaring 38 Moose Squisher");
+      e.power = 380;
+      e.sound_level = 10;
+      c.fit_engine(e);
+      console.log(c);
+      return expect(c.engine.name).toBe("Roaring 38 Moose Squisher");
     });
   });
 
--- app/src/mySpec.coffee
+++ app/src/mySpec.coffee
@@ -6,19 +6,48 @@ describe "how to run a test", ->
     expect(add(5,3)).toBe(8)
 
 describe "how to build a class", ->
-  it "car should have 4 wheels...", ->
+  
+  class Car
+    wheels: 4
+    engine: undefined
 
-    class Car
-      wheels: 4
-      engine: undefined
+    constructor: (name) ->
+      @name = name
 
-      constructor: (name) ->
-        @name = name
+    fit_engine: (engine)  =>
+      @engine = engine
+  
+  class Engine
+    power: 300
+    sound_level: 9
 
-      fit_engine: (engine)  =>
-        @engine = engine
+    constructor: (name) ->
+      @name = name
 
+  it "car should return correct default properties...", ->
     c = new Car
-
     expect(c.wheels).toBe(4)
+
+  it "should return correct name", ->
+    c = new Car
+    c.name = "Mobile"
+    expect(c.name).toBe("Mobile")
+
+  it "should have an engine and and return its name", ->
+    # Car spec
+    c = new Car
+    c.name = "The Beast"
+    
+    # Engine spec
+    e = new Engine("Roaring 38 Moose Squisher")
+    e.power = 380
+    e.sound_level = 10
+    
+    # Fit engine
+    c.fit_engine(e)
+   
+    # Report on work done
+    console.log(c)
+
+    expect(c.engine.name).toBe("Roaring 38 Moose Squisher")
 
